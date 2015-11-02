b2d1cbb43dc3744069b845012d7958a26161b11e
Sam Burns <sam.burns.uk@gmail.com>
2015-11-02 10:37:06 +0000

    Bug fixes to Event Handling

--- src/BehatContextFileUsage/CoverageCalculator.php
+++ src/BehatContextFileUsage/CoverageCalculator.php
@@ -1,10 +1,10 @@
 <?php
 namespace BehatContextFileUsage;
 
+use Behat\Testwork\EventDispatcher\Event\AfterExerciseCompleted;
+use Behat\Testwork\EventDispatcher\Event\AfterSuiteTested as AfterSuiteTestedEvent;
+use Behat\Testwork\EventDispatcher\Event\BeforeSuiteTested as BeforeSuiteTestedEvent;
 use Behat\Testwork\EventDispatcher\TestworkEventDispatcher as EventDispatcher;
-use Behat\Testwork\EventDispatcher\Event\LifecycleEvent as Event;
-use Behat\Testwork\EventDispatcher\Event\SuiteTested as SuiteTestedEvent;
-use Behat\Testwork\EventDispatcher\Event\ExerciseCompleted as ExerciseCompletedEvent;
 
 class CoverageCalculator
 {
@@ -22,58 +22,38 @@ class CoverageCalculator
     /**
      * @throws \InvalidArgumentException
      *
-     * @param Event           $event
-     * @param string          $eventName
-     * @param EventDispatcher $eventDispatcher
+     * @param BeforeSuiteTestedEvent $event
+     * @param string                 $eventName
+     * @param EventDispatcher        $eventDispatcher
      */
-    public function beNotifiedOfSuiteStartingEvent(Event $event, $eventName, EventDispatcher $eventDispatcher)
+    public function beNotifiedOfSuiteStartingEvent(BeforeSuiteTestedEvent $event, $eventName, EventDispatcher $eventDispatcher)
     {
-        $this->verifyEventType(SuiteTestedEvent::BEFORE, $eventName);
-
         $suiteName = $event->getSuite()->getName();
-
         $this->phpUnitCodeCoverageTool->startRecordingCodeUsageWithTestName($suiteName);
     }
 
     /**
      * @throws \InvalidArgumentException
      *
-     * @param Event           $event
-     * @param string          $eventName
-     * @param EventDispatcher $eventDispatcher
+     * @param AfterSuiteTestedEvent $event
+     * @param string                $eventName
+     * @param EventDispatcher       $eventDispatcher
      */
-    public function beNotifiedOfSuiteFinishingEvent(Event $event, $eventName, EventDispatcher $eventDispatcher)
+    public function beNotifiedOfSuiteFinishingEvent(AfterSuiteTestedEvent $event, $eventName, EventDispatcher $eventDispatcher)
     {
-        $this->verifyEventType(SuiteTestedEvent::AFTER, $eventName);
-
         $this->phpUnitCodeCoverageTool->stopRecordingCodeUsage();
     }
 
     /**
      * @throws \InvalidArgumentException
      *
-     * @param Event           $event
-     * @param string          $eventName
-     * @param EventDispatcher $eventDispatcher
+     * @param AfterExerciseCompleted $event
+     * @param string                 $eventName
+     * @param EventDispatcher        $eventDispatcher
      */
-    public function beNotifiedOfExerciseFinishedEvent(Event $event, $eventName, EventDispatcher $eventDispatcher)
+    public function beNotifiedOfExerciseFinishedEvent(AfterExerciseCompleted $event, $eventName, EventDispatcher $eventDispatcher)
     {
-        $this->verifyEventType(ExerciseCompletedEvent::AFTER, $eventName);
-
         $this->phpUnitCodeCoverageTool->stopRecordingCodeUsage();
         $this->phpUnitCodeCoverageTool->publishCodeUsageReport();
-    }
-
-    /**
-     * @throws \InvalidArgumentException
-     *
-     * @param string $expectedEventType
-     * @param string $actualEventType
-     */
-    private function verifyEventType($expectedEventType, $actualEventType)
-    {
-        if ($actualEventType !== $expectedEventType) {
-            throw new \InvalidArgumentException('Listener called with wrong event');
-        }
     }
 }
